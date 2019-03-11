5436840e44569f55a1448e1c3b7ebb83e526a7e5
Milen Radkov <milenradkov@me.com>
2019-03-11 18:53:48 +0200

    location added wip

--- drone.js
+++ drone.js
@@ -1,5 +1,19 @@
 const math = require('mathjs')
 
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  static distance(a, b) {
+    const dx = a.x - b.x;
+    const dy = a.y - b.y;
+
+    return Math.hypot(dx, dy);
+  }
+}
+
 class Drone {
     constructor(id, battery) {
         this.id = id;
@@ -7,14 +21,25 @@ class Drone {
         this.loaded = false;
         this.batteryDrownRateEmpty = 1;
         this.batteryDrownRateLoaded = 1;
+        this.location = new Point(12, -123);
 
         // log
         console.log(`Initiated drone with the following settings:`);
         console.log(`${JSON.stringify(this)}`);
     }
 
+    set setNewLocation(location) {
+      this.location = location;
+    }
+
     // Start drowning the battery when the drone is flying
-    fly(distance) {
+    fly(newLocation) {
+
+        let distance = Point.distance(this.location, newLocation);
+
+        console.log(`The distance is calculated: ${distance}`);
+
+        // replace loop with for every point
         for(let i = 0; i <= distance; i++) {
           if (this.battery == 0) {
             console.log('Drone battery is dead!');
@@ -24,10 +49,12 @@ class Drone {
           if (this.loaded == true) {this.battery -= this.batteryDrownRateLoaded;}
           
           this.battery -= this.batteryDrownRateEmpty;
+          this.location = 
 
           // log
           console.log('Drone is flying towards the destination!');
           console.log(`Battery level: ${this.battery}`);
+          console.log(`Location: ${this.location}`);
         }
     }
 
@@ -48,4 +75,4 @@ class Drone {
 }
 
 let drone = new Drone(1, 100);
-drone.fly(12);
+drone.fly(new Point(301, 123));
